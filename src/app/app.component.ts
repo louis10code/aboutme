5e9bc70ae707840cef2d03a1448b75e5127cdc5d
louis10code <louis10code@gmail.com>
2024-03-09 21:13:52 +0800

    add some code to introduce myself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aboutme';
+  Name = 'louis10code';
+  title = 'About Me';
+  programmingLanguage = 'Programming Language';
+  
 }
